e636d0441d870df5af7efde01b58da6cdf00b4aa
Danny Pranoto <danny.pranoto@veritrans.co.id>
2016-01-29 18:49:58 +0700

    Add two click get token params

--- vt-IOSlib-SampleStore/ViewController.swift
+++ vt-IOSlib-SampleStore/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UIWebViewDelegate {
     let totalPrice = "10000"
     let vtDirect = VTDirect()
     let cardDetails = VTCardDetails()
-    var jsonData: JSON = []
+    var requestData: JSON?
     
     @IBOutlet weak var creditCardNumber: UITextField!
     @IBOutlet weak var expMonth: UITextField!
@@ -34,25 +34,33 @@ class ViewController: UIViewController, UIWebViewDelegate {
         // get token and charge transaction
         acquiredCardDetails()
         vtDirect.card_details = cardDetails
-        vtDirect.getToken{(data, exception) -> Void in
-            if let jsonData: JSON = JSON(data: data) {
-                self.jsonData = jsonData
-                if jsonData["redirect_url"] != nil {
-                    //Displaying redirect url in Web View - 3DS
-                    let webView:UIWebView = UIWebView(frame: CGRectMake(0, 10, 320, 320))
-                    webView.loadRequest(NSURLRequest(URL: NSURL(string: jsonData["redirect_url"].stringValue)!))
-                    webView.delegate = self
-                    webView.scalesPageToFit = true;
-                    webView.multipleTouchEnabled = false;
-                    webView.contentMode = UIViewContentMode.ScaleAspectFit;
-                    self.view.addSubview(webView)
+        vtDirect.getToken{(responseData : NSData!, ex : NSException!) -> Void in
+            if (ex == nil) {
+                //checking response data nil or not
+                if (responseData != nil) {
+                    var requestData = JSON(data: responseData);
+                    self.requestData = requestData
+                    if (requestData["redirect_url"] != nil) {
+                        //Displaying redirect url in Web View - 3DS
+                        let webView:UIWebView = UIWebView(frame: CGRectMake(0, 10, 320, 320))
+                        webView.loadRequest(NSURLRequest(URL: NSURL(string: requestData["redirect_url"].stringValue)!))
+                        webView.delegate = self
+                        webView.scalesPageToFit = true;
+                        webView.multipleTouchEnabled = false;
+                        webView.contentMode = UIViewContentMode.ScaleAspectFit;
+                        self.view.addSubview(webView)
+                    }
+                    else {
+                        //charge transaction - non 3DS
+                        self.chargeRequest("token-id=\(requestData["token_id"].stringValue)")
+                    }
+                } else {
+                    print("Unable to retrieve token")
                 }
-                else {
-                    //charge transaction - non 3DS
-                    self.chargeRequest("token-id=\(jsonData["token_id"].stringValue)")
-                }
+
             } else {
-                print("Unable to retrieve token")
+                //Something is wrong, get details message by print ex.reason
+                print(ex.reason)
             }
         }
     }
@@ -61,8 +69,13 @@ class ViewController: UIViewController, UIWebViewDelegate {
         // register user's card and exchange it with saved_token_id
         acquiredCardDetails()
         vtDirect.card_details = cardDetails
-        vtDirect.registerCard{(savedToken, exception) -> Void in
-            self.sendRegisteredCardToken(savedToken)
+        vtDirect.registerCard{(responseData: NSData!, ex: NSException!) -> Void in
+            if(ex == nil) {
+                // checking response data nil or not
+                self.sendRegisteredCardToken(responseData)
+            } else {
+                print(ex.reason);
+            }
         }
     }
     
@@ -79,7 +92,7 @@ class ViewController: UIViewController, UIWebViewDelegate {
     func sendRegisteredCardToken(serverResponse: NSData!) {
         // register user's card and exchange it with saved_token_id
         var jsonBody = JSON(data:serverResponse!)
-        jsonBody["user_id"] = "A"
+        jsonBody["user_id"] = "PUT_YOUR_USER_ID_HERE"
         if let body = jsonBody.rawString() {
             let request = generatePostRequest(merchantUrl, bodyData: body)
             let session = NSURLSession.sharedSession()
@@ -98,28 +111,22 @@ class ViewController: UIViewController, UIWebViewDelegate {
         // charge transaction after user has successfully authenticated by 3D Secure
         if(webView.request?.URL!.absoluteString.rangeOfString("callback") != nil){
             webView.removeFromSuperview()
-            let bodyData = "token-id=\(self.jsonData["token_id"].stringValue)&price=\(totalPrice)"
+            let bodyData = "token-id=\(self.requestData!["token_id"].stringValue)&price=\(totalPrice)"
             chargeRequest(bodyData)
         }
     }
     
-    func chargeRequest(bodyData: String) {
+    func chargeRequest(responseData: String) {
         // sending charge request to merchant's server
-        let request = generatePostRequest(merchantUrl, bodyData: bodyData)
+        let request = generatePostRequest(merchantUrl, bodyData: responseData)
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) -> Void in
             if (error == nil) {
+                // if your server has successfully charge the transaction
                 let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
                 print("Body: \(strData!)")
-                let json = JSON(data:data!)
-                let success = json["status"].stringValue
-                if (success == "success"){
-                    let trxId = json["body"]["transaction_id"]
-                    print("Success to Charging data with transaction id\(trxId)")
-                } else {
-                    print("Failed to Charge")
-                }
             } else {
+                // if your server has failed charge the transaction
                 print("Error: \(error!.localizedDescription)")
             }
         })
